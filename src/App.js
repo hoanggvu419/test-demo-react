9e9c50083e1c535d2b271b9614618b492cc0c5ba
Hoang Vu <vuviethoang041005@gmail.com>
2025-10-27 23:58:27 +0700

    điều hướng

--- src/App.js
+++ src/App.js
@@ -1,44 +1,26 @@
-import logo from './logo.svg';
 import './App.scss';
-import { useDispatch, useSelector } from 'react-redux';
-import { increaseCounter, decreaseCounter } from './redux/action/counterAction';
-import MyComponent from './components/MyComponent';
-import React from 'react';
+import Header from './components/Header/Header';
+import {Outlet, Link } from 'react-router-dom';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className='app-container'>
-      Hello World with Vu Viet Hoang 
-      <MyComponent></MyComponent>
+const App = () => {
+
+  return (
+    <div className="app-container">
+      <div className='header-container'>
+        <Header />
+      </div>
+      <div className='main-container'>
+         <div className='sidenav-container'>
+
+         </div>
+         <div className='app-content'>
+          <Outlet />
+         </div>
+      </div>
+
+
     </div>
-    );
-  }
+  ); 
 }
-
-
-// const App = () => {
-//   const count = useSelector(state => state.counter.count);
-//   const dispatch = useDispatch();
-
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
 
 export default App;
